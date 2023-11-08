1c035f83290fdd88a470a8fa997507ed7c18be97
René Gál <galrene@fit.cvut.cz>
2023-11-08 17:40:03 +0100

    cv05_flash: enumerate endpoints and get max lun

--- cv05_flash/main.c
+++ cv05_flash/main.c
@@ -17,46 +17,59 @@ int main(int argc, char **argv) {
   // Get plug&play info
   struct libusb_device_descriptor desc;
   libusb_get_device_descriptor(device, &desc);
-
-  // Check if device if USB flash memory
-  if (desc.bDeviceClass != LIBUSB_CLASS_MASS_STORAGE) {
-    fprintf(stderr, "Device not a USB flash memory\n");
-    return 1;
-  }
-
-  // Zjistime počet endpointů
-  unsigned int num_endpoints = desc.bNumEndpoints;
-
+  // get number of endpoints from interface descriptor
+  struct libusb_config_descriptor *config;
+  libusb_get_config_descriptor(libusb_get_device(device), 0, &config);
+  int num_endpoints = config->interface[0].altsetting[0].bNumEndpoints;
   // Pro každý endpoint zjistíme typ a směr
   for (unsigned int i = 0; i < num_endpoints; i++) {
     struct libusb_endpoint_descriptor endpoint;
     libusb_get_endpoint_descriptor(device, i, &endpoint);
-
-    printf("Endpoint %d: typ %d, směr %d\n", i, endpoint.bEndpointType, endpoint.bEndpointAddress & LIBUSB_ENDPOINT_DIR_MASK);
+    printf("Endpoint %d: typ %d, směr %d\n", i, endpoint.bDescriptorType, endpoint.bEndpointAddress & LIBUSB_ENDPOINT_DIR_MASK);
   }
 
-  // Zjistime počet logických zařízení
-  unsigned int max_lun = 0;
-  libusb_control_transfer(device, LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_DEVICE, USB_REQ_GET_MAX_LUN, 0, 0, &max_lun, sizeof(max_lun), 1000);
+  // Get Max LUN
+  unsigned char max_lun;
+  libusb_control_transfer(   device,
+                             LIBUSB_ENDPOINT_IN
+                           | LIBUSB_REQUEST_TYPE_CLASS 
+                           | LIBUSB_RECIPIENT_INTERFACE,
+                           0xFE,
+                           0,
+                           0,
+                           &max_lun,
+                           1,
+                           1000);
+  printf("Max LUN: %d\n", max_lun);
 
-  printf("Počet logických zařízení: %d\n", max_lun);
+  // // create control block wrapper
+  // struct libusb_control_setup setup;
+  // setup.bmRequestType = LIBUSB_ENDPOINT_OUT | LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE;
+  // setup.bRequest = 0x06;
+  // setup.wValue = 0x0200;
+  // setup.wIndex = 0;
+  // setup.wLength = 0;
+  // unsigned char status;
+  // libusb_control_transfer(device, setup.bmRequestType, setup.bRequest, setup.wValue, setup.wIndex, &status, 1, 1000);
+  // printf("Status: %d\n", status);
 
-  // Načítáme a zapisujeme sektory
-  for (unsigned int lun = 0; lun < max_lun; lun++) {
-    // Načtení sektoru
-    unsigned char buf[512];
-    libusb_bulk_transfer(device, endpoint.bEndpointAddress | LIBUSB_ENDPOINT_IN, buf, sizeof(buf), &buf[0], 1000);
 
-    // Zobrazení načteného sektoru
-    for (unsigned int i = 0; i < sizeof(buf); i++) {
-      printf("%02x ", buf[i]);
-    }
-    printf("\n");
+  // // Načítáme a zapisujeme sektory
+  // for (unsigned int lun = 0; lun < max_lun; lun++) {
+  //   // Načtení sektoru
+  //   unsigned char buf[512];
+  //   libusb_bulk_transfer(device, endpoint.bEndpointAddress | LIBUSB_ENDPOINT_IN, buf, sizeof(buf), &buf[0], 1000);
 
-    // Zapsání sektoru
-    memset(buf, 0xff, sizeof(buf));
-    libusb_bulk_transfer(device, endpoint.bEndpointAddress | LIBUSB_ENDPOINT_OUT, buf, sizeof(buf), &buf[0], 1000);
-  }
+  //   // Zobrazení načteného sektoru
+  //   for (unsigned int i = 0; i < sizeof(buf); i++) {
+  //     printf("%02x ", buf[i]);
+  //   }
+  //   printf("\n");
+
+  //   // Zapsání sektoru
+  //   memset(buf, 0xff, sizeof(buf));
+  //   libusb_bulk_transfer(device, endpoint.bEndpointAddress | LIBUSB_ENDPOINT_OUT, buf, sizeof(buf), &buf[0], 1000);
+  // }
 
   // Zavřeme zařízení
   libusb_close(device);
